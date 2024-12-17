1cca9ab0bc649076b22ffdda54f404430e014542
DavidVMuela <dom.villagomez@yavirac.edu.ec>
2024-12-17 18:06:12 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // Importar los módulos necesarios
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
